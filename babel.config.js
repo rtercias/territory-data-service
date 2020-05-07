8576f8b0414853826bd5ae6f592a94f6dfd86d88
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-05-07 12:25:45 -0400

    resolve babel preset

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
 
   const presets = [
     [
-      "@babel/preset-env",
+      require.resolve("@babel/preset-env"),
       {
         "targets": {
           "node": "10"
