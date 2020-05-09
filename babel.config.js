b12209f286e24324f7e7e3377cb4b4ebc44f4716
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-05-09 08:41:35 -0400

    see console logs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
 
+  console.log('cwd', process.cwd());
+  console.log('__dirname', __dirname);
+
   const presets = [
     [
       '@babel/preset-env',
