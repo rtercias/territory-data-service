ce0fefbb3efe106c7b4206e08f3fdee8e31d9870
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-06-21 00:33:22 -0400

    ignore files and test env var function

--- src/functions.js
+++ src/functions.js
@@ -6,5 +6,9 @@ const server = gqlServer();
 // Graphql api
 // https://us-east1-territory-data-service.cloudfunctions.net/api/
 const api = https.onRequest(server);
-
-export {api};
+const helloEnvVariables = https.onRequest((req, res) => {
+  let server = process.env.TERRITORY_SERVER;
+  let userid = process.env.TERRITORY_USERID;
+  res.status(200).send(`Function My env vars: ${server}, ${userid}`);
+});
+export { api, helloEnvVariables };
