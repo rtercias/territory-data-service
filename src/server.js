59ca037e5058a4c2cc38077d9a734bfa902abee0
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-07-04 15:45:43 -0400

    fix headers list

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ export function gqlServer() {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header("Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, Referer, User-Agent, X-Requested-With, Content-Type, Accept');
     next();
   });
 
