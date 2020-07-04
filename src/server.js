003d8500873be2a33d5bd766dd0e12dd3d25d050
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-07-04 14:45:47 -0400

    add Authorization header

--- src/server.js
+++ src/server.js
@@ -41,7 +41,8 @@ export function gqlServer() {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
