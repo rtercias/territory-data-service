bf3718609b09560de04e4a9297f6121581d78a59
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-07-05 00:47:50 -0400

    limit allowed origins

--- src/functions.js
+++ src/functions.js
@@ -10,7 +10,11 @@ const server = gqlServer();
 // https://us-east1-territory-data-service.cloudfunctions.net/api/
 const api = https.onRequest((req, res) => {
   cors(req, res, () => {
-    res.header('Access-Control-Allow-Origin', '*');
+    const allowedOrigins = ['http://localhost:8080', 'http://192.168.1.205:8080', 'https://foreignfield.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.indexOf(origin) > -1){
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Authorization, Origin, Referer, User-Agent, X-Requested-With, Content-Type, Accept');
     server();
