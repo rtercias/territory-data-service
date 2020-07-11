f136e9c98563f4a17abcffcbb70be3adabea6aac
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-07-11 17:56:41 -0400

    test sample function without cors

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,8 @@
 const { https } = require('firebase-functions');
 const { gqlServer } = require('./server');
-const cors = require('cors')({
-  origin: true
-});
+// const cors = require('cors')({
+//   origin: true
+// });
 
 const server = gqlServer();
 
@@ -10,8 +10,8 @@ const server = gqlServer();
 // https://us-east1-territory-data-service.cloudfunctions.net/api/
 const api = https.onRequest(server);
 const sample = https.onRequest((req, res) => {
-  cors(req, res, () => {
-    res.send('Passed.');
-  });
+  // cors(req, res, () => {
+  res.send('Passed.');
+  // });
 });
 export { api, sample };
