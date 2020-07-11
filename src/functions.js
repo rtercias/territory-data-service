640f4a6d5a74f98d575fb7717951821abb5559bd
Ryan Tercias <ryan.tercias@MacBook-Pro-2.local>
2020-07-11 12:05:10 -0400

    revert another cors fix attempt

--- src/functions.js
+++ src/functions.js
@@ -8,25 +8,5 @@ const server = gqlServer();
 
 // Graphql api
 // https://us-east1-territory-data-service.cloudfunctions.net/api/
-const api = https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const allowedOrigins = ['http://localhost:8080', 'http://192.168.1.205:8080', 'https://foreignfield.com'];
-    const origin = req.headers.origin;
-
-    res.set('Access-Control-Allow-Credentials', 'true');
-
-    if (allowedOrigins.indexOf(origin) > -1){
-      res.set('Access-Control-Allow-Origin', origin);
-    }
-
-    if (req.method === 'OPTIONS') {
-      res.set('Access-Control-Allow-Methods', 'GET, POST');
-      res.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-      res.set('Access-Control-Max-Age', '3600');
-      res.status(204).send('');
-    } else {
-      res.send(server(req, res));
-    }
-  });
-});
+const api = https.onRequest(server);
 export { api };
