ec3749601cf7d2b1dfda6e0fcf9b603869c2294e
Ryan Tercias <ryan.tercias@MacBook-Pro.fios-router.home>
2021-05-19 10:16:58 -0400

    allow to include campaign mode in S-13 report query

--- src/async/reports.js
+++ src/async/reports.js
@@ -3,16 +3,18 @@ import { toArray } from 'lodash';
 
 class ReportsAsync {
   async getAssignmentRecords(congId, campaignMode) {
+    let sql = '';
     if (campaignMode === undefined) {
-      // get cong
-      const resultCong = await conn.query(`SELECT * FROM congregations WHERE id=${congId}`);
-      const cong = resultCong[0];
-      campaignMode = cong.campaign || 0;
+      sql = `SELECT * FROM territorycheckouts_pivot
+      where congregationid=${congId}
+      order by territory_id, timestamp`;
+    } else {
+      sql = `SELECT * FROM territorycheckouts_pivot
+      where congregationid=${congId} and COALESCE(campaign, 0)=${campaignMode}
+      order by territory_id, timestamp`;
     }
 
-    return toArray(await conn.query(`SELECT * FROM territorycheckouts_pivot
-      where congregationid=${congId} and COALESCE(campaign, 0)=${campaignMode}
-      order by territory_id, timestamp`));
+    return toArray(await conn.query(sql));
   }
 }
 
