55f43d9d82b2c10cd302b1e8bdc57e02c79c5865
Ryan Tercias <ryan.tercias@gmail.com>
2021-12-07 16:40:25 -0500

    use json file instead

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = require('../../firebase-service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
