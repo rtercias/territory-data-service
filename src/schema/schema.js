7dcbd3977fa946d233d2c08f1d8eb0a2de159b08
Ryan Tercias <ryan.tercias@gmail.com>
2021-12-08 17:15:41 -0500

    add twilio and sendSMS function

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -52,6 +52,7 @@ import {
   queryResolvers as phoneQueryResolvers,
   mutationResolvers as phoneMutationResolvers,
 } from './types/Phone';
+import { sendSMSMessage } from '../utils/Twilio';
 
 const RootQuery = gql`
   type RootQuery {
@@ -122,6 +123,7 @@ const Mutation = gql`
     addPublisher(publisher: PublisherInput!): Publisher
     updatePublisher(publisher: PublisherInput!): Publisher
     deletePublisher(id: Int!): Boolean
+    sendSMS(text: String!, number: String!): String
   }
 `;
 
@@ -147,7 +149,11 @@ export const resolvers = {
   ),
 
   Mutation: merge (
-    {},
+    {
+      sendSMS: async (root, { text, number }) => {
+        return await sendSMSMessage(text, number);
+      },
+    },
     publisherMutationResolvers,
     territoryMutationResolvers,
     activityLogMutationResolvers,
