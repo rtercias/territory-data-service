16b17e81df9688fe84ee69ce62ccde6fa20c292e
Ryan Tercias <ryan.tercias@gmail.com>
2021-12-22 13:40:13 -0500

    move twilio config lookup to server.js

--- src/utils/Twilio.js
+++ src/utils/Twilio.js
@@ -1,13 +1,10 @@
-const twilio = require('twilio');
 const { config } = require('firebase-functions');
-const accountSid = config().api.twilio_account_sid;
-const authToken = config().api.twilio_auth_token;
 const from = config().api.twilio_phone_number;
-const client = twilio(accountSid, authToken);
+import { twilioClient } from '../server';
 
 export const sendSMSMessage = async (text, number) => {
   try {
-    const { status } = await client.messages.create({ body: text, from, to: number });
+    const { status } = await twilioClient.messages.create({ body: text, from, to: number });
     return status;
   } catch (e) {
     throw e;
