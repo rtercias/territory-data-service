860bd2a6bb42fc4c5023e84a54b3d44a5c905816
Ryan Tercias <ryan.tercias@gmail.com>
2022-03-30 10:15:28 -0400

    revert connection limit back to 10

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ const { validateFirebaseIdToken } = require('./utils/Firebase');
 const twilio = require('twilio');
 
 export const conn = mysql.createPool({
-  connectionLimit: 100,
+  connectionLimit: 10,
   ssl: { rejectUnauthorized: false }, // TODO: add SSL certificate file here (see https://github.com/mysqljs/mysql#ssl-options)
   host: config().api.territory_server,
   user: config().api.territory_userid,
